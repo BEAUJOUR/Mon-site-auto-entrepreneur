15cf2a5e7e1a8f88d35f4ca942b7ff93a247c3a1
BEAUJOUR <beaujourbicool@gmail.com>
2025-09-18 20:36:16 +0200

    changer le logo

--- src/components/BJDProTitle.jsx
+++ src/components/BJDProTitle.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function BJDProTitle() {
   return (
    <div className="text-center py-6">
-  <span className="text-5xl md:text-2xl font-bold uppercase tracking-[0.2em] text-text-dark font-futuristic drop-shadow-lg">
+  <span className="text-2xl md:text-2xl font-bold uppercase tracking-[0.2em] text-text-dark font-futuristic drop-shadow-lg">
     <span className="text-primary">BJD</span>
     <span className="text-text-dark">-HOME</span>
   </span>
